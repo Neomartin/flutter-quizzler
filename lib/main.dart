0be5ede7a039e530adae566bf0bbdb88216c49fe
Martin Rodriguez <martin.rodriguez@naranja.com>
2023-02-06 05:57:51 -0300

    Score list and state working

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Quizzler());
+
+class Quizzler extends StatelessWidget {
+  const Quizzler({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: const SafeArea(
+          child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              child: QuizPage()),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({Key? key}) : super(key: key);
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [
+    const Icon(
+        color: Colors.green,
+        Icons.check
+    ),
+    const Icon(
+        color: Colors.red,
+        Icons.close
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        const Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'This is where my question will go',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.green
+              ),
+              onPressed: () {
+                setState(() {
+                  scoreKeeper.add(const Icon(color: Colors.green, Icons.check),);
+                });
+
+              },
+              child: const Text(
+                'True',
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red
+              ),
+              onPressed: () {
+                setState(() {
+                  scoreKeeper.add(
+                    const Icon(color: Colors.red, Icons.close),
+                  );
+                });
+
+              },
+              child: const Text(
+                'False',
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper
+        )
+      ],
+    );
+  }
+}
